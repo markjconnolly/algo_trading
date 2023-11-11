2b286043c73a9809580efc436836bf8c6269ea60
Mark Connolly <mark.jude.connolly@gmail.com>
2023-11-10 20:25:12 -0500

    Tic-tac-toe implemented

--- test_app/src/App.js
+++ test_app/src/App.js
@@ -7,21 +7,25 @@ import { useState } from 'react';
 ////////
 export default function Game() {
 
-  const [xIsNext, setXisNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
-  const currentSquares = history[history.length - 1];
+  const xIsNext = currentMove % 2 === 0;
 
+  const currentSquares = history[currentMove];
+
+  //
   function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares])
-    setXisNext(!xIsNext)
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory)
+    setCurrentMove(nextHistory.length-1)
   }
 
+  //
   function jumpTo(nextMove) {
     setCurrentMove(nextMove);
-    setXisNext(nextMove % 2 === 0);
   }
 
+  //
   const moves = history.map(
     (squares, move) => {
       let description;
@@ -39,11 +43,12 @@ export default function Game() {
     }
   );
 
+  //
   return (
     <div className='game'>
       
       <div className='game-board'>
-        <Board xIsNext={xIsNext} squares={currentSquares} onPlay = {handlePlay}/>
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
       </div>
       
       <div className='game-info'>
